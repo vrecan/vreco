fe63287182287928ee59256f6c5c66a84ec08aee
Ben Aldrich <vrecan@gmail.com>
2022-05-17 19:47:22 -0600

    sse events work but need to fix server, need to broadcast to all clients and improve shutdown handling

--- routes/routes.go
+++ routes/routes.go
@@ -2,7 +2,9 @@ package routes
 
 import (
 	"errors"
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 
 	"html/template"
@@ -10,6 +12,8 @@ import (
 	"github.com/Masterminds/sprig"
 	"github.com/labstack/echo/v4"
 )
+
+var messageChan chan string
 
 // Define the template registry struct
 type TemplateRegistry struct {
@@ -33,9 +37,13 @@ func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c 
 }
 
 func Setup(e *echo.Echo) {
+	if messageChan == nil {
+		messageChan = make(chan string)
+	}
 	templates := make(map[string]*template.Template)
 	templates["home.html"] = template.Must(template.ParseFiles("templates/pages/home.html", "templates/base.html"))
-	templates["clicked.html"] = template.Must(template.ParseFiles("templates/components/clicked.html"))
+	templates["about.html"] = template.Must(template.ParseFiles("templates/pages/about.html", "templates/base.html"))
+	templates["clicked.html"] = template.Must(template.ParseFiles("templates/partials/clicked.html"))
 	e.Renderer = &TemplateRegistry{
 		templates: templates,
 	}
@@ -51,7 +59,68 @@ func Setup(e *echo.Echo) {
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "home.html", map[string]interface{}{})
 	})
+	e.GET("/about", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "about.html", map[string]interface{}{})
+	})
 	e.POST("/clicked", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "clicked.html", map[string]interface{}{})
 	})
+
+	e.GET("/chatroom", func(c echo.Context) error {
+		handler := handleSSE()
+		handler(c.Response().Writer, c.Request())
+		return nil
+	})
+
+	e.POST("/sendChat", func(c echo.Context) error {
+		msg := c.FormValue("msg")
+
+		if messageChan != nil && msg != "" {
+
+			// send the message through the available channel
+			select {
+			case messageChan <- msg:
+				log.Println("message sent to channel: ", msg)
+			default:
+				log.Println("no one listening for messages, not sending: ", msg)
+			}
+		}
+		return c.String(http.StatusOK, "success")
+	})
+
+}
+
+func handleSSE() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Get handshake from client")
+		// prepare the header
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		// instantiate the channel
+
+		// prepare the flusher
+		flusher, _ := w.(http.Flusher)
+
+		// trap the request under loop forever
+		for {
+
+			select {
+
+			// message will received here and printed
+			case msg := <-messageChan:
+				fmt.Println("Sending message through chatroom", msg)
+				fmt.Fprintf(w, "data: %s\n\n", msg)
+				flusher.Flush()
+
+			// connection is closed then defer will be executed
+			case <-r.Context().Done():
+				return
+
+			}
+		}
+
+	}
 }
